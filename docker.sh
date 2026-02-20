63eabc1321f1090f513b8ae77860484504abfb41
JavaApp3.0 <devcanarycnl+noreply@users.noreply.github.com>
2026-02-20 11:59:58 +0530

    dockerfile repo is changed

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 
 sudo apt update -y
 
